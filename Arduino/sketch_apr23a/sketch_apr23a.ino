fcfc55984589da3319e1ab1ce684f3b553e74257
Ashish Kumar <ashish.kumar85297@gmail.com>
2023-05-05 06:23:43 +0530

    updated

--- Arduino/sketch_apr23a/sketch_apr23a.ino
+++ Arduino/sketch_apr23a/sketch_apr23a.ino
@@ -22,19 +22,25 @@
 #define DHTTYPE DHT11
 #include <ArduinoJson.h>
 #include <Wire.h>
+//#include <WiFi.h>
 // #include <Adafruit_BMP085.h>
+// #include <SFE_BMP180.h>
+// #include <Wire.h>
+
+// Adafruit_BMP085 bmp;
+// #define I2C_SCL 12
+// #define I2C_SDA 13
 
 #include <NTPClient.h>
 WiFiUDP ntpUDP;
-
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 19800);
+
 
 const char* ssid = "Narzo";
 
 const char* password = "qwe123qwe";
 
 DHT dht(0, DHTTYPE);
-
 
 void setup() {
   Serial.begin(9600);
@@ -56,20 +62,20 @@ void setup() {
 
   dht.begin();
   timeClient.begin();
-   while(!timeClient.update()) {
-     timeClient.forceUpdate();
-   }
-//  Serial.end();
+//   while(!timeClient.update()) {
+//     timeClient.forceUpdate();
+//     
+//   }
+  //Serial.end();
 }
 
 void send_sensor_data(float temperature, float humidity, float water_level, String formattedTime, int hours, int minutes, int second) {
 
-  HTTPClient http;
+//  HTTPClient http;
   WiFiClient client;
-//---------------
-//  http.begin(client, "http://127.0.0.1:8000/sensordata/");
+
+//  http.begin(client, "http://14-ce3xxx:8000/sensordata/");
 //  http.addHeader("Content-Type", "application/json");
-//  //-----------
 
   StaticJsonDocument<2000> jsonDocument;
   jsonDocument["temperature"] = temperature;
@@ -83,15 +89,14 @@ void send_sensor_data(float temperature, float humidity, float water_level, Stri
   String jsonString;
   serializeJson(jsonDocument, jsonString);
   Serial.println(jsonString);
-  //--------------
-  int httpResponseCode = http.POST(jsonString);
-  if (httpResponseCode == 200) {
-    Serial.println("Data sent successfully");
-  } else {
-    Serial.print("Error sending data. HTTP response code: ");
-    Serial.println(httpResponseCode);
-  }
-  http.end();
+//  int httpResponseCode = http.POST(jsonString);
+//  if (httpResponseCode == 200) {
+//    Serial.println("Data sent successfully");
+//  } else {
+//    Serial.print("Error sending data. HTTP response code: ");
+//    Serial.println(httpResponseCode);
+//  }
+//  http.end();
 
 
 
@@ -99,6 +104,9 @@ void send_sensor_data(float temperature, float humidity, float water_level, Stri
 
 void loop() {
   timeClient.update();
+//    while(!timeClient.update()) {
+//    timeClient.forceUpdate();
+//  }
   float water_level = analogRead(A0);
 //  Serial.print("A0 Water_level = ");
 //  Serial.println(water_level);
@@ -106,7 +114,7 @@ void loop() {
 
   String formattedTime = timeClient.getFormattedTime();
 
-//  Serial.println(formattedTime);
+  Serial.println(formattedTime);
   int hours = timeClient.getHours();
   int minutes = timeClient.getMinutes();
   int second = timeClient.getSeconds();
@@ -115,17 +123,17 @@ void loop() {
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
-//  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED) {
 
-  send_sensor_data(temperature, humidity, water_level, formattedTime, hours, minutes, second);
-//  }
+    send_sensor_data(temperature, humidity, water_level, formattedTime, hours, minutes, second);
+  }
 
 //  Serial.print("D3 humidity = ");
 //  Serial.println(humidity);
-  // Serial.print("%  ");
+//  // Serial.print("%  ");
 //  Serial.print("D3 temperature = ");
 //  Serial.println(temperature);
-  // Serial.println(" C");
+//  // Serial.println(" C");
 
   delay(3000);
 
--- readme.md
+++ readme.md
@@ -1,23 +1,31 @@
-Install xgboost and djangorestframework
-
- 1. sudo pip install xgboost
- 2. pip install djangorestframework
-
-Before running arduino code
-    
-     1. Change the ssid and password to your wifi network.
-     2. Change the ip address to the ip address of your computer.
-     3. Change the port to the port you want to use.
-        go to utils.py and change the port to the same port you used in the arduino code.
+## Arduino IDE setup For NodeMCU
 
 Go to file->preferences and add below url in the "Additional Boards Manager URLs" field.
+
 ```
 http://arduino.esp8266.com/stable/package_esp8266com_index.json
 ```
+
 Then go to Tools->Board->Boards Manager and search for esp8266 and install it.
 
+## Hardware connection Required
+
+For DHT11 sensor:
+
+1. connect the VCC of DHT sensor to Vin of NodeMCU
+2. connect the GND of DHT sensor to GND of NodeMCU
+3. connect the data pin of DHT sensor to D3 of NodeMCU
+
+For Water level sensor:
+
+1. Connect the Signal pin(S) of water level sensor to A0 of NodeMCU
+2. Connect the VCC of water level sensor to Vin of NodeMCU
+3. Connect the GND of water level sensor to GND of NodeMCU
+
 ## 2. Install the required libraries in Arduino IDE if you don't have them already
+
 Go to Sketch->Include Library->Manage Libraries and install the following libraries:
+
 - Adafruit Unified Sensor
 - Adafruit BME280 Library
 - Adafruit_BusIO
@@ -31,17 +39,32 @@ Go to Sketch->Include Library->Manage Libraries and install the following librar
 - NTPClient
 - Time
 
+## Upload the code
 
-## 4. Upload the code
-Connect your ESP8266 to your computer and upload the code. If you have any issues,contact me.
+- connect the USB cable to the computer and upload the code to the NodeMCU using Arduino IDE.
+- Select the board as NodeMCU 0.9 (ESP-12E Module) and the port to which the NodeMCU is connected to the computer.
 
-To run the django server
+## Before running arduino code
+
+1. Change the ssid and password to your wifi network.
+2. Change the ip address to the ip address of your computer.
+3. Change the port to the port you want to use.
+go to utils.py and change the port to the same port you used in the arduino code.
+
+## Installation and setup required for Django server and ML model
+
+### To run Django server and ML model
 
 1. clone the repository
 2. cd mysite
-3. python manage.py runserver
+3. Install xgboost,django and djangorestframework
+   1. `pip install Django==4.2.1`
+   2. `sudo pip install xgboost`
+   3. `pip install djangorestframework`
 
-go to http://localhost:8000/sensordata/ to start sensor data being stored in the database.
+4. run `python manage.py runserver`
 
-go to http://localhost:8000/home/ to forecast temperature, humidity and and water level.
+- go to http://localhost:8000/sensordata/ to start sensor data being stored in the database.
+
+- go to http://localhost:8000/home/ to forecast temperature, humidity and and water level.
 Enter the 24 hour format of the time you want to predict for and click on submit.
